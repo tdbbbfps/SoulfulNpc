dd1f5d27abd4785898e7660319ffea07b4bb1e2b
tdbbb <tdbbb0427@gmail.com>
2026-03-11 08:57:15 +0800

    feat: handle player's inputs.

--- godot/entities/player/input_handler.gd
+++ godot/entities/player/input_handler.gd
@@ -6,8 +6,13 @@ extends Node
 func _physics_process(delta: float) -> void:
 	var dir : float = Input.get_axis("left", "right")
 	movement_component.move(dir, delta)
+
+func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("jump") and actor.is_on_floor():
-		movement_component.jump(delta)
+		movement_component.jump()
 	if Input.is_action_just_released("jump") and actor.velocity.y < -50.0:
 		movement_component.jumpcut()
-	
+	if Input.is_action_just_pressed("drop_down"):
+		movement_component.toggle_dropdown(false)
+	if Input.is_action_just_released("drop_down"):
+		movement_component.toggle_dropdown(true)
