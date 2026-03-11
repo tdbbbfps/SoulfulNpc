b145ddff4ca71966b216aaca0628716de0c07dd2
tdbbb <tdbbb0427@gmail.com>
2026-03-11 08:52:18 +0800

    feat: add jumpcut and dropdown.

--- godot/entities/components/movement_component.gd
+++ godot/entities/components/movement_component.gd
@@ -14,12 +14,15 @@ func move(dir : float, delta : float) -> void:
 	else:
 		actor.velocity.x = 0.0
 
-func jump(delta : float) -> void:
+func jump() -> void:
 	actor.velocity.y = actor.jump_velocity
-	
+
 func jumpcut() -> void:
 	actor.velocity.y *= 0.5
 
 func apply_gravity(delta : float) -> void:
 	if not actor.is_on_floor():
 		actor.velocity.y += actor.get_gravity().y * delta
+
+func toggle_dropdown(toggle_on : bool) -> void:
+	actor.set_collision_mask_value(5, toggle_on)
